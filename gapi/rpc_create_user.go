f42e0df51508d06be38137bd68773ebd0141694c
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-04-24 00:35:56 -0400

    Implement background worker with Redis task queue

--- gapi/rpc_create_user.go
+++ gapi/rpc_create_user.go
@@ -38,6 +38,7 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 		}
 		return nil, status.Errorf(codes.Internal, "failed to create user: %s", err)
 	}
+	//Send verify email
 	rsp := &pb.CreateUserResponse{
 		User: convertUser(user),
 	}
