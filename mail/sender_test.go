92cb731a7f610c288bb4d005c80fa2889bcfabf5
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-04-25 03:24:42 -0400

    Send Email function

---
+++ mail/sender_test.go
@@ -0,0 +1,26 @@
+package mail
+
+import (
+	"testing"
+
+	"github.com/BinhNguyenDang/simplebank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSendEmailWithGmail(t *testing.T) {
+	config, err := util.LoadConfig("..")
+	require.NoError(t, err)
+
+	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+
+	subject := "A test Email"
+	content := `
+	<h1>Hello World!</h1>
+	<p>This is a message from me</p>
+	`
+	to := []string{"simplebank16@gmail.com"}
+	attachFiles := []string{"../README.md"}
+
+	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+	require.NoError(t, err)
+}
--- util/config.go
+++ util/config.go
@@ -19,6 +19,9 @@ type Config struct {
 	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
 	RefreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
 	RedisAddress         string        `mapstructure:"REDIS_ADDRESS"`
+	EmailSenderName      string        `mapstructure:"EMAIL_SENDER_NAME"`
+	EmailSenderAddress   string        `mapstructure:"EMAIL_SENDER_ADDRESS"`
+	EmailSenderPassword  string        `mapstructure:"EMAIL_SENDER_PASSWORD"`
 }
 
 // LoadConfig reads configuration from file or enviroment variables
