292ec7733eba2a942fbc53cfa8fe9cfff4620336
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-05-08 04:27:13 -0400

    add unit tests for the update_user_rpc

--- gapi/main_test.go
+++ gapi/main_test.go
@@ -1,16 +1,20 @@
 package gapi
 
 import (
+	"context"
+	"fmt"
 	"testing"
 	"time"
 
 	db "github.com/BinhNguyenDang/simplebank/db/sqlc"
+	"github.com/BinhNguyenDang/simplebank/token"
 	"github.com/BinhNguyenDang/simplebank/util"
 	"github.com/BinhNguyenDang/simplebank/worker"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/metadata"
 )
 
-func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDistributor ) *Server {
+func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDistributor) *Server {
 	config := util.Config{
 		TokenSymmetricKey:   util.RandomString(32),
 		AccessTokenDuration: time.Minute,
@@ -19,4 +23,16 @@ func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDist
 	require.NoError(t, err)
 
 	return server
+}
+
+func newContextWithBearerToken(t *testing.T, tokenMaker token.Maker, username string, duration time.Duration) context.Context {
+	accessToken, _, err := tokenMaker.CreateToken(username, duration)
+	require.NoError(t, err)
+	bearerToken := fmt.Sprintf("%s %s", authorizationBearer, accessToken)
+	md := metadata.MD{
+		authorizationHeader: []string{
+			bearerToken,
+		},
+	}
+	return metadata.NewIncomingContext(context.Background(), md)
 }
